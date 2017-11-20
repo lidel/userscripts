e82313e4f5f0fad0b937e861e3c4d273783e0267
Marcin Rataj <lidel@lidel.org>
2017-11-20 13:02:50 +0100

    fix: compatibility with Greasemonkey 4
    Context: http://www.greasespot.net/2017/09/greasemonkey-4-for-script-authors.html

--- gmail-fixed-font.user.js
+++ gmail-fixed-font.user.js
@@ -5,31 +5,47 @@
 // @icon           https://mail.google.com/favicon.ico
 // @run-at         document-start
 // @description    Custom fixed-font in Gmail messages
-// @version        1.2.3
+// @version        1.3.0
 // @license        CC0; https://creativecommons.org/publicdomain/zero/1.0/
 // @downloadURL    https://raw.githubusercontent.com/lidel/userscripts/master/gmail-fixed-font.user.js
 // @homepageURL    https://github.com/lidel/userscripts
-// @grant          none
+// @require        https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @grant          GM_addStyle
+// @grant          GM.addStyle
 // ==/UserScript==
 
-var fontName = 'Inconsolata';
-var fontSubset = 'latin';
+const fontName = 'Inconsolata';
+const fontSubset = 'latin';
 
 // dragons below this line
-var fontCss = 'font-family: \'' + fontName + '\', monospace !important;';
+const fontCss = 'font-family: \'' + fontName + '\', monospace !important;';
 // plain-text messages
-var css = '.ii, .Ak {' + fontCss + '}';
+let css = '.ii, .Ak {' + fontCss + '}';
 // editor
 css += '.editable {' + fontCss + '}';
 // load
-var heads = document.getElementsByTagName('head');
+const heads = document.getElementsByTagName('head');
+// load font
 if (heads.length > 0) {
-  var link = document.createElement('link');
+  const link = document.createElement('link');
   link.rel = 'stylesheet';
   link.href = '//fonts.googleapis.com/css?family=' + fontName.replace(/\ /g, '+') + '&subset=' + fontSubset;
   heads[0].appendChild(link);
-  var node = document.createElement('style');
+  const node = document.createElement('style');
   node.type = 'text/css';
   node.appendChild(document.createTextNode(css));
   heads[0].appendChild(node);
+}
+// attach style
+if (typeof GM_addStyle != "undefined") {
+    GM_addStyle(css);
+} else if (typeof addStyle != "undefined") {
+    addStyle(css);
+} else {
+  if (heads.length > 0) {
+    const node = document.createElement('style');
+    node.type = 'text/css';
+    node.appendChild(document.createTextNode(css));
+    heads[0].appendChild(node);
+  }  
 }
